4a7ccd093a0271a3f3ab5f16be72abac42e146dd
Sagar Goyal <sagargoyal2015@gmail.com>
2021-09-20 22:54:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom';
+import {Layout, Typography, Space} from 'antd';
+import Navbar from './components/Navbar';
+import Exchanges from './components/Exchanges';
+import Homepage from './components/Homepage';
+import News from './components/News';
+import CryptoDetails from './components/CryptoDetails';
+import Cryptocurrencies from './components/Cryptocurrencies';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import './App.css'; 
+
+const App = () => {
+    return (
+        <div className="app">
+            <div className="navbar">
+                <Navbar/>
+            </div>
+            <div className="main">
+                <Layout>
+                    <div className="routes">
+                        <Switch>
+                            <Route exact path="/" component={Homepage}/>
+                            <Route exact path="/exchanges" component={Exchanges}/>
+                            <Route exact path="/cryptocurrencies" component={Cryptocurrencies}/>
+                            <Route exact path="/crypto/:coinId" component={CryptoDetails}/>
+                            <Route exact path="/news" component={News}/>
+                        </Switch>
+                    </div>
+                </Layout>
+            
+            <div className="footer">
+                <Typography.Title level={5} style={{ color:"white", textAlign:"center" }}>
+                    Cryptoverse <br/>
+                    All rights reserved © 2069
+                </Typography.Title>
+                <Space>
+                    <Link to='/'>Home</Link>
+                    <Link to='/exchanges'>Exchanges</Link>
+                    <Link to='/news'>News</Link>
+                </Space>
+            </div>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
